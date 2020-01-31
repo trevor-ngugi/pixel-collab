6ac7d58f319f7e5832db33ef55622a1b518050a8
trevor ngugi <ngugitrevor@gmail.com>
2020-01-31 14:08:30 +0300

    add image model

--- pixel/models.py
+++ pixel/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image=models.CharField(max_length =30)
+    image_name=models.CharField(max_length =30)
+    description=models.CharField(max_length =30)
