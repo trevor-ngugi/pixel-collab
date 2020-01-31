fc4050e0ac775837067e4f99f0ab2d3af9f9b081
trevor ngugi <ngugitrevor@gmail.com>
2020-01-31 14:15:18 +0300

    add location  model

--- pixel/models.py
+++ pixel/models.py
@@ -5,3 +5,12 @@ class Image(models.Model):
     image=models.CharField(max_length =30)
     image_name=models.CharField(max_length =30)
     description=models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.image_name
+
+    # class Meta:                                            for ordering
+    #     ordering = ['first_name']
+
+class Location(models.Model):
+    place=models.CharField(max_length =30)
