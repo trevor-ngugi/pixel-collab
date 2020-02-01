4d63c0893e2bf350b04d4a58a8edee9ce6098be4
trevor ngugi <ngugitrevor@gmail.com>
2020-02-01 12:56:31 +0300

    add test for location

--- pixel/tests.py
+++ pixel/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import  Place,Category,Image
 
 # Create your tests here.
+
+class PlaceTestClass(TestCase):
+    def setUp(self):
+        self.nairobi=Place(location='nairobi')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Place))
